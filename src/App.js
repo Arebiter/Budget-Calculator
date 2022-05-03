b3baad74ca132c54c3172dd347903e6942158844
Arebiter <pasandharmasena@gmail.com>
2022-05-03 01:33:52 -0400

    Import components and pass in state as props

--- src/App.js
+++ src/App.js
@@ -1,50 +1,44 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import { AnnualExpenses } from "./components/annualExpenses";
 import './App.css';
+import { MonthlyExpenses } from "./components/monthlyExpenses";
+import { TotalSavings } from "./components/totalSavings";
 
 function App() {
-  const { totalIncome, setTotalIncome } = useState(0);
-  const { incomeAfterDeductions, setIncomeAfterDeductions } = useState(0);
-  const { totalMonthlyExpense, setTotalMonthlyExpenses } = useState(0);
-  const { totalSavings, setTotalSavings } = useState(0);
-  const { remainingMonthly, setRemainingMonthly } = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [incomeAfterDeductions, setIncomeAfterDeductions] = useState(0);
+  const [totalMonthlyExpenses, setTotalMonthlyExpenses] = useState(0);
+  const [totalSavings, setTotalSavings] = useState(0);
+  const [remainingMonthly, setRemainingMonthly] = useState(0);
+
+  const handleIncome = (e) => {
+    setTotalIncome(Number(e.target.value));
+    // console.log(totalIncome)
+  }
 
 
   return (
     <div className="App">
       <div>
         Total Income Before Tax
-        <input type="text" value={totalIncome} placeholder="0"></input>
+        <input type="text" value={totalIncome} placeholder="0" onChange={handleIncome} name="Total Income" />
       </div>
-      <div>
-        Annual Expenses
-        <p>FICA percentage</p>
-        <p>Exemptions</p>
-        <p>Standard Deductions</p>
-        <p>Tax Bracket</p>
-        <div>
-          Tax Rates
-          <p>Federal</p>
-          <p>State</p>
-          <p>Local</p>
-        </div>
-      </div>
-      <div>
-        Monthly Expenses
-        <p>Rent/housing</p>
-        <p>Food</p>
-        <p>transportation</p>
-        <p>Medical</p>
-        <p>personal</p>
-        <p>clothing</p>
-        <p>utilities</p>
-        <p>Extra</p>
-      </div>
-      <div>
-        Savings
-        <p>Emergency</p>
-        <p>Investments</p>
-        <p>Retirement</p>
-      </div>
+      <AnnualExpenses
+        totalIncome={totalIncome}
+        setIncomeAfterDeductions={setIncomeAfterDeductions}
+        incomeAfterDeductions={incomeAfterDeductions}
+      />
+      <MonthlyExpenses
+        incomeAfterDeductions={incomeAfterDeductions}
+        totalMonthlyExpenses={totalMonthlyExpenses}
+        setTotalMonthlyExpenses={setTotalMonthlyExpenses}
+      />
+      <TotalSavings
+        totalSavings={totalSavings}
+        setTotalSavings={setTotalIncome}
+        totalMonthlyExpenses={totalMonthlyExpenses}
+        setTotalMonthlyExpenses={setTotalMonthlyExpenses}
+      />
     </div>
   );
 }
