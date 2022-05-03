ce2ce3286a555083f249685c3d476ccad6bddea8
Arebiter <pasandharmasena@gmail.com>
2022-05-02 23:17:37 -0400

    outlined tentative requirements for budget calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
 
 function App() {
+  const { totalIncome, setTotalIncome } = useState(0);
+  const { incomeAfterDeductions, setIncomeAfterDeductions } = useState(0);
+  const { totalMonthlyExpense, setTotalMonthlyExpenses } = useState(0);
+  const { totalSavings, setTotalSavings } = useState(0);
+  const { remainingMonthly, setRemainingMonthly } = useState(0);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        Total Income Before Tax
+        <input type="text" value={totalIncome} placeholder="0"></input>
+      </div>
+      <div>
+        Annual Expenses
+        <p>FICA percentage</p>
+        <p>Exemptions</p>
+        <p>Standard Deductions</p>
+        <p>Tax Bracket</p>
+        <div>
+          Tax Rates
+          <p>Federal</p>
+          <p>State</p>
+          <p>Local</p>
+        </div>
+      </div>
+      <div>
+        Monthly Expenses
+        <p>Rent/housing</p>
+        <p>Food</p>
+        <p>transportation</p>
+        <p>Medical</p>
+        <p>personal</p>
+        <p>clothing</p>
+        <p>utilities</p>
+        <p>Extra</p>
+      </div>
+      <div>
+        Savings
+        <p>Emergency</p>
+        <p>Investments</p>
+        <p>Retirement</p>
+      </div>
     </div>
   );
 }
